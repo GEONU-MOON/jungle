a7d81bc0a5eb1f3acf48b2b74a152f4b6f5bc2de
GEONU-MOON <moondy2209@naver.com>
2024-01-19 13:29:42 +0900

    memo_ver2.0

--- memozang/app.py
+++ memozang/app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # MongoDB 클라이언트 설정
-client = MongoClient('mongodb+srv://moondy2209:A3ykBYjy9iGaeUF4@cluster0.t0cskbu.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://moondy2209:비번 뭐게@cluster0.t0cskbu.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbjungle
 
 @app.route('/')
@@ -26,5 +26,24 @@ def post_memo():
 
     return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
 
+@app.route('/edit_memo', methods=['POST'])
+def edit_memo():
+    original_title = request.form['originalTitle']
+    edited_title = request.form['editedTitle']
+    edited_text = request.form['editedText']
+
+    db.memos.update_one({'title': original_title}, {'$set': {'title': edited_title, 'text': edited_text}})
+    
+    return jsonify({'result': 'success'})
+
+@app.route('/delete_memo', methods=['POST'])
+def delete_memo():
+    index = int(request.form['index'])
+    memo = list(db.memos.find({}, {'_id': 1}))[index]
+    db.memos.delete_one({'_id': memo['_id']})
+    return jsonify({'result': 'success'})
+
 if __name__ == '__main__':  
     app.run('0.0.0.0', port=5000, debug=True)
+
+
